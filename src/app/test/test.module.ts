39e005c1d66a243aa94f06ca6b2248aecab5a102
Eric Hopper <ehopper@avidxchange.com>
2022-05-27 07:56:37 -0600

    organizes services and directives into their own dirs

--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from '../foo/foo.component';
-import { ColorTextDirective } from '../color-text.directive';
+import { ColorTextDirective } from '../directives/color-text.directive';
 
 
 
