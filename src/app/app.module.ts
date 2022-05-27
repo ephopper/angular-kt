4f5de7026d4bdb088d0cd39d2acbab7e7cff401a
Eric Hopper <ehopper@avidxchange.com>
2022-05-27 08:27:56 -0600

    fixes imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LifecycleModule } from './lifecycle/lifecycle.module';
+import { LifecycleModule } from './test/lifecycle/lifecycle.module';
 import { TestModule } from './test/test.module';
 
 @NgModule({
