8672c9e5f89be2fa64e3d6c8e87f10565bca8d07
Eric Hopper <ehopper@avidxchange.com>
2022-07-08 13:43:05 -0600

    nests facade specs by type

--- src/app/state/users/users.facade.spec.ts
+++ src/app/state/users/users.facade.spec.ts
@@ -17,33 +17,37 @@ describe('Users Facade', () => {
     store = TestBed.inject(MockStore);
   });
 
-  it(
-    'should select users',
-    marbles((m) => {
-      const users = m.cold('a', { a: [] });
-      store.overrideSelector(UserSelectors.selectUsers, [] )
-      m.expect(facade.users$).toBeObservable(users);
-    })
-  );
+  describe('Selectors', () => {
+    it(
+      'should select users',
+      marbles((m) => {
+        const users = m.cold('a', { a: [] });
+        store.overrideSelector(UserSelectors.selectUsers, []);
+        m.expect(facade.users$).toBeObservable(users);
+      })
+    );
 
-  it(
-    'should select loading',
-    marbles((m) => {
-      const loading = m.cold('a', { a: true });
-      store.overrideSelector(UserSelectors.selectUsersLoading, true);
-      m.expect(facade.isLoading$).toBeObservable(loading);
-    })
-  );
-
-  it('should dispatch a userRequest action', () => {
-    const spy = jest.spyOn(store, 'dispatch');
-    facade.getUsers();
-    expect(spy).toHaveBeenCalledWith(UserActions.userRequest());
+    it(
+      'should select loading',
+      marbles((m) => {
+        const loading = m.cold('a', { a: true });
+        store.overrideSelector(UserSelectors.selectUsersLoading, true);
+        m.expect(facade.isLoading$).toBeObservable(loading);
+      })
+    );
   });
 
-  it('should dispatch a userDelete action', () => {
-    const spy = jest.spyOn(store, 'dispatch');
-    facade.deleteUsers(1);
-    expect(spy).toHaveBeenCalledWith(UserActions.userDelete({ userId: 1 }));
+  describe('Methods', () => {
+    it('should dispatch a userRequest action', () => {
+      const spy = jest.spyOn(store, 'dispatch');
+      facade.getUsers();
+      expect(spy).toHaveBeenCalledWith(UserActions.userRequest());
+    });
+
+    it('should dispatch a userDelete action', () => {
+      const spy = jest.spyOn(store, 'dispatch');
+      facade.deleteUsers(1);
+      expect(spy).toHaveBeenCalledWith(UserActions.userDelete({ userId: 1 }));
+    });
   });
 });
